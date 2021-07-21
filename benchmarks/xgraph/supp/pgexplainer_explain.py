f041543a45b5463d67f480cbdae3ce63854c63a0
Peibo Li <49071801+neolifer@users.noreply.github.com>
2021-07-22 00:44:25 +1000

    Update pgexplainer_explain.py

--- benchmarks/xgraph/supp/pgexplainer_explain.py
+++ benchmarks/xgraph/supp/pgexplainer_explain.py
@@ -14,6 +14,12 @@ import os
 import argparse
 from PGExplainer.load_dataset import get_dataset, get_dataloader
 from torch_geometric.utils import add_remaining_self_loops, remove_self_loops
+from itertools import product
+from tqdm import tqdm
+from dig.xgraph.method import PGExplainer
+from dig.xgraph.evaluation import XCollector
+import random
+import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', default='GCN2', dest='gnn models')
 parser.add_argument('--model_name', default='GCN')
@@ -41,27 +47,26 @@ def index_to_mask(index, size):
     mask[index] = 1
     return mask
 
-def split_dataset(dataset):
-    indices = []
-    num_classes = 4
-    train_percent = 0.7
-    for i in range(num_classes):
-        index = (dataset.data.y == i).nonzero().view(-1)
-        index = index[torch.randperm(index.size(0))]
-        indices.append(index)
+# def split_dataset(dataset, num_classes):
+#     indices = []
+#     train_percent = 0.7
+#     for i in range(num_classes):
+#         index = (dataset.data.y == i).nonzero().view(-1)
+#         index = index[torch.randperm(index.size(0))]
+#         indices.append(index)
+#
+#     train_index = torch.cat([i[:int(len(i) * train_percent)] for i in indices], dim=0)
+#
+#     rest_index = torch.cat([i[int(len(i) * train_percent):] for i in indices], dim=0)
+#     rest_index = rest_index[torch.randperm(rest_index.size(0))]
+#
+#     dataset.data.train_mask = index_to_mask(train_index, size=dataset.data.num_nodes)
+#     dataset.data.val_mask = index_to_mask(rest_index[:len(rest_index) // 2], size=dataset.data.num_nodes)
+#     dataset.data.test_mask = index_to_mask(rest_index[len(rest_index) // 2:], size=dataset.data.num_nodes)
 
-    train_index = torch.cat([i[:int(len(i) * train_percent)] for i in indices], dim=0)
-
-    rest_index = torch.cat([i[int(len(i) * train_percent):] for i in indices], dim=0)
-    rest_index = rest_index[torch.randperm(rest_index.size(0))]
-
-    dataset.data.train_mask = index_to_mask(train_index, size=dataset.data.num_nodes)
-    dataset.data.val_mask = index_to_mask(rest_index[:len(rest_index) // 2], size=dataset.data.num_nodes)
-    dataset.data.test_mask = index_to_mask(rest_index[len(rest_index) // 2:], size=dataset.data.num_nodes)
-
-    dataset.data, dataset.slices = dataset.collate([dataset.data])
-
-    return dataset
+    # dataset.data, dataset.slices = dataset.collate([dataset.data])
+    #
+    # return dataset
 
 dataset = get_dataset(parser)
 # dim_node = dataset.num_node_features
@@ -70,14 +75,13 @@ dataset.data.x = dataset.data.x.to(torch.float32)
 # dataset.data.x = dataset.data.x[:, :1]
 # dataset.data.y = dataset.data.y[:, 2]
 dim_node = dataset.num_node_features
+
 dim_edge = dataset.num_edge_features
 # num_targets = dataset.num_classes
 num_classes = dataset.num_classes
-
-splitted_dataset = split_dataset(dataset)
-splitted_dataset.data.mask = splitted_dataset.data.test_mask
-splitted_dataset.slices['mask'] = splitted_dataset.slices['train_mask']
-dataloader = DataLoader(splitted_dataset, batch_size=1, shuffle=False)
+# print(dataset.data)
+# sys.exit()
+# splitted_dataset = split_dataset(dataset, num_classes)
 
 
 
@@ -125,90 +129,94 @@ model.to(device)
 # Fidelity: 0.2849
 # Sparsity: 0.9605
 # lr: 0.001
-torch.manual_seed(42)
-import random
-random.seed(0)
-import numpy as np
-np.random.seed(0)
+
+torch.backends.cudnn.benchmark = True
+
+# tensor(0.1331) [0.34, 1, 1.5, 0.001]
+# tensor(0.1334) [0.3, 2.5, 1, 0.001]
 coff_sizes = [0.01,0.02,0.03,0.04]
 coff_ents = [0.5,1,2,2.5,3]
 coff_preds = [0.5,1,1.5,2,2.5,3]
 lrs = [0.001, 0.01, 0.005]
-from itertools import product
-from tqdm import tqdm
-from dig.xgraph.method import PGExplainer
+
+
+
 best_fidelity = 0
 best_parameters = []
 for coff_size, coff_ent, coff_pred, lr in tqdm(iterable= product(coff_sizes, coff_ents, coff_preds, lrs),
                                                total= len(list(product(coff_sizes, coff_ents, coff_preds, lrs)))):
+# for coff_size, coff_ent, coff_pred, lr in product(coff_sizes, coff_ents, coff_preds, lrs):
+    torch.manual_seed(42)
+    random.seed(0)
+    np.random.seed(0)
     explainer = PGExplainer(model, lr = lr, in_channels=3*dim_hidden,
                             device=device, explain_graph=False, num_hops = 3, epochs = 20,
                             coff_size= coff_size, coff_ent= coff_ent, coff_pred = coff_pred).cuda()
-    explainer.train_explanation_network(splitted_dataset, batch = batch)
+    explainer.train_explanation_network(dataset.data.cuda(), batch = batch)
     torch.save(explainer.state_dict(), 'tmp1.pt')
     state_dict = torch.load('tmp1.pt')
 
     explainer.load_state_dict(state_dict)
 
 
-
-
-# node_indices = [i for i in range(400,700,5)]
-# from dig.xgraph.method.pgexplainer import PlotUtils
-# plotutils = PlotUtils(dataset_name='ba_community')
-# data = dataset[0].cuda()
-# node_idx = node_indices[0]
-# walks, masks, related_preds = \
-#             explainer(data, node_idx=node_idx, y=data.y, top_k=6)
-# explainer.visualization(data, edge_mask=masks[0], top_k=6, plot_utils=plotutils, node_idx=node_idx, vis_name = f'fig/pgexplainer_bacom_gcn{node_idx}.pdf')
-# sys.exit()
-# for i in range(len(node_indices)):
-#     explainer = explainer.cuda()
-#     explainer.model = explainer.model.cuda()
-#     data=data.cuda()
-#     node_idx = node_indices[i]
-#     walks, masks, related_preds = \
-#         explainer(data, node_idx=node_idx, y=data.y, top_k=6)
-#     explainer.visualization(data, edge_mask=masks[0], top_k=6, plot_utils=plotutils, node_idx=node_idx, vis_name = f'fig/pgexplainer_bacom_gcn{node_idx}.pdf')
-# sys.exit()
+    # data = dataset[0].cuda()
+    # node_indices = torch.where(data.test_mask == True)[0].tolist()
+    # from dig.xgraph.method.pgexplainer import PlotUtils
+    # plotutils = PlotUtils(dataset_name='ba_community')
+    #
+    # node_idx = node_indices[6]
+    # walks, masks, related_preds = \
+    #             explainer(data, node_idx=node_idx, y=data.y, top_k=6)
+    # explainer.visualization(data, edge_mask=masks[0], top_k=6, plot_utils=plotutils, node_idx=node_idx, vis_name = f'fig/pgexplainer_bacom_gcn/pgexplainer_bacom_gcn{node_idx}.pdf')
+    # sys.exit()
+    # for i in range(len(node_indices)):
+    #     explainer = explainer.cuda()
+    #     explainer.model = explainer.model.cuda()
+    #     data=data.cuda()
+    #     node_idx = node_indices[i]
+    #     walks, masks, related_preds = \
+    #         explainer(data, node_idx=node_idx, y=data.y, top_k=6)
+    #     explainer.visualization(data, edge_mask=masks[0], top_k=6, plot_utils=plotutils, node_idx=node_idx, vis_name = f'fig/pgexplainer_bacom_gcn{node_idx}.pdf')
+    # sys.exit()
 
 # --- Create data collector and explanation processor ---
-    from dig.xgraph.evaluation import XCollector
+
     x_collector = XCollector()
 
     ## Run explainer on the given model and dataset
     index = -1
-    for i, data in enumerate(dataloader):
-        for j, node_idx in enumerate([i for i in range(400,700,5)]):
-            index += 1
-            # print(f'explain graph {i} node {node_idx}')
-            data.to(device)
+    data = dataset.data
+    for j, node_idx in enumerate(torch.where(data.test_mask == True)[0].tolist()):
+        index += 1
+        # print(f'explain graph {i} node {node_idx}')
+        data.to(device)
 
-            if torch.isnan(data.y[0].squeeze()):
-                continue
+        if torch.isnan(data.y[0].squeeze()):
+            continue
 
-            walks, masks, related_preds= \
-                explainer(data, node_idx=node_idx, y=data.y, top_k=6)
+        walks, masks, related_preds= \
+            explainer(data, node_idx=node_idx, y=data.y, top_k=6)
 
-            x_collector.collect_data(masks, related_preds)
+        x_collector.collect_data(masks, related_preds)
 
-            # if you only have the edge masks without related_pred, please feed sparsity controlled mask to
-            # obtain the result: x_processor(data, masks, x_collector)
-            if index >= 99:
-                break
-
+        # if you only have the edge masks without related_pred, please feed sparsity controlled mask to
+        # obtain the result: x_processor(data, masks, x_collector)
         if index >= 99:
             break
+
+    if index >= 99:
+        break
     print('hyper parameters:\n'
           f'coff_size: {coff_size}\n'
           f'coff_ent: {coff_ent}\n'
-          f'coff_pred: {coff_pred}\n'
-         f'lr: {lr}')
+          f'coff_pred: {coff_pred}')
     fidelity = x_collector.fidelity
+    fidelity_inv = x_collector.fidelity_inv
     if fidelity > best_fidelity:
         best_fidelity = fidelity
         best_parameters = [coff_size, coff_ent, coff_pred, lr]
     print(f'Fidelity: {fidelity:.4f}\n'
+          f'Fidelity_inv: {fidelity_inv:.4f}\n'
           f'Sparsity: {x_collector.sparsity:.4f}')
 
 print(best_fidelity, best_parameters)
