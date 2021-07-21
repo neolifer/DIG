974eafa8a905dfbb59fd2d4739e52d3149b7686c
Peibo Li <49071801+neolifer@users.noreply.github.com>
2021-07-21 12:17:13 +1000

    Update __init__.py

--- dig/xgraph/method/__init__.py
+++ dig/xgraph/method/__init__.py
@@ -1,8 +1,4 @@
-from .deeplift import *
-from .gnn_gi import *
-from .gnn_lrp import *
-from .gnnexplainer import *
-from .gradcam import *
+
 from .pgexplainer import *
 from .subgraphx import *
 from .graphmask import *
